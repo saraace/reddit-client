9aedbbf39c4398f45d28b6246fff2e1dfd151fd0
Sara Acevedo <sara.g.acevedo@gmail.com>
2021-11-15 22:21:31 -0500

    populating post list with posts from redux

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,11 @@
+import { useAppSelector } from "../../services/redux/hooks";
+import { selectSubredditLoading, selectSubredditPosts } from "../../services/redux/reducers/subreddit";
+
 const PostList = () => {
-	return <div>Post List</div>;
+	const posts = useAppSelector(selectSubredditPosts);
+	const loadingPosts = useAppSelector(selectSubredditLoading);
+
+	return <div>{loadingPosts ? <div>Loading</div> : posts.map((post) => <div key={post.id}>{post.title}</div>)}</div>;
 };
 
 export default PostList;
--- src/interfaces/Post.ts
+++ src/interfaces/Post.ts
@@ -1,4 +1,5 @@
 export default interface Post {
+	id: string;
 	title: string;
 	thumbnail: string;
 }
