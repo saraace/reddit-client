a9d55f32fe62b803d072d828abeab85e20038f28
Sara Acevedo <sara.g.acevedo@gmail.com>
2021-11-16 18:13:21 -0500

    adjusting layout to accomidate tall post detail images

--- src/containers/Layout/Layout.styles.tsx
+++ src/containers/Layout/Layout.styles.tsx
@@ -12,6 +12,7 @@ export const GlobalStyles = createGlobalStyle`
 export const Container = styled.div`
 	display: flex;
 	height: 100vh;
+	overflow: hidden;
 `;
 
 export const Sidebar = styled.div`
@@ -40,10 +41,32 @@ export const Sidebar = styled.div`
 export const Content = styled.div`
 	flex-grow: 1;
 	padding: 0 20px;
+	display: flex;
+	flex-direction: column;
 `;
 
 export const SubredditTitle = styled.div`
 	border-bottom: 2px solid ${(props) => props.theme.colors.light100};
 `;
 
-export const SelectedPost = styled.div``;
+export const SelectedPost = styled.div`
+	flex-grow: 1;
+	padding: 0 0 40px;
+	overflow-y: scroll;
+	scrollbar-width: none;
+
+	&::-webkit-scrollbar {
+		width: 6px;
+	}
+	&::-webkit-scrollbar-track {
+		background: transparent;
+	}
+	&::-webkit-scrollbar-thumb {
+		background: transparent;
+		border-radius: 6px;
+	}
+	&:hover::-webkit-scrollbar-thumb,
+	&:active::-webkit-scrollbar-thumb {
+		background-color: ${(props) => props.theme.colors.inputBg};
+	}
+`;
