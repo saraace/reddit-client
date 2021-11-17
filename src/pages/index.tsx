433065502ccf337021ead1d49ddef5c17db068c5
Sara Acevedo <sara.g.acevedo@gmail.com>
2021-11-16 19:04:33 -0500

    dispatch an action on load to fetch posts if subreddit is not defined

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../services/redux/hooks";
-import { loadPosts, selectSubredditPosts } from "../services/redux/reducers/subreddit/subreddit";
+import { loadPosts, selectSubredditName, selectSubredditPosts } from "../services/redux/reducers/subreddit/subreddit";
 import PostDetail from "../components/PostDetail/PostDetail";
 import Layout from "../containers/Layout/Layout";
 import { selectSelectedPost } from "../services/redux/reducers/app/app";
@@ -10,13 +10,14 @@ import { selectSelectedPost } from "../services/redux/reducers/app/app";
 const Home: NextPage = () => {
 	const dispatch = useAppDispatch();
 	const selectedPost = useAppSelector(selectSelectedPost);
+	const subredditName = useAppSelector(selectSubredditName);
 	const posts = useAppSelector(selectSubredditPosts);
 
 	useEffect(() => {
-		if (posts.length <= 0) {
+		if (subredditName === "" && posts.length <= 0) {
 			dispatch(loadPosts("Damnthatsinteresting"));
 		}
-	}, [dispatch, posts]);
+	}, [dispatch, posts, subredditName]);
 
 	return (
 		<>
